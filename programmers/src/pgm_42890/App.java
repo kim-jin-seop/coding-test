ab29156e3fbbe2ec5c548e5cb364fec81c6a22c8
kim-jin-seop <33629459+kim-jin-seop@users.noreply.github.com>
2022-03-03 01:23:38 +0900

    후보키

---
+++ programmers/src/pgm_42890/App.java
@@ -0,0 +1,9 @@
+package pgm_42890;
+
+public class App {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.solution(new String[][]{{"100","ryan","music","2"},{"200","apeach","math","2"},{"300","tube","computer","3"}
+        ,{"400","con","computer","4"},{"500","muzi","music","3"},{"600","apeach","music","2"}});
+    }
+}
