592dbbb3547779c7a189144f298772cb79a65dd3
kim-jin-seop <33629459+kim-jin-seop@users.noreply.github.com>
2022-03-09 23:12:19 +0900

    배달

---
+++ programmers/src/pgm_12978/App.java
@@ -0,0 +1,8 @@
+package pgm_12978;
+
+public class App {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.solution(5,new int[][]{{1,2,1},{1,3,2},{2,3,2},{3,4,3},{3,5,2},{3,5,3},{5,6,1}},3);
+    }
+}
